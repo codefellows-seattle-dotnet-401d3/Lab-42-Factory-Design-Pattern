805b648090982530551a1421ce2f78fc0a293ce0
wtnerb <wtnerb@gmail.com>
2018-05-17 08:30:22 -0700

    partial print to console

--- FactoryDesignPattern/FactoryDesignPattern/Program.cs
+++ FactoryDesignPattern/FactoryDesignPattern/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace FactoryDesignPattern
 {
@@ -6,6 +7,16 @@ namespace FactoryDesignPattern
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Starting to assemble your documents. Your name is?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Thank you. Processing ...");
+            Console.WriteLine("writing Resume ... building resume writer");
+            Thread.Sleep(800);
+            ResumeWriter Tolkien = new ResumeWriter();
+            Document resume = Tolkien.Write(name);
+            Console.WriteLine($"{nameof(Tolkien)} has written a resume for {resume.Name}");
+            Console.WriteLine($"resume filetype is {resume.FileExtension}");
+            Conwole.WriteLine($"Previous work history: you have worked at {resume.Work}")
             Console.WriteLine("Hello World!");
         }
     }
